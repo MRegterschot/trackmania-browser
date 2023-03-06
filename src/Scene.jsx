b38264e943f3eac1ef9d423f72e8d642698f28b6
MRegterschot <marijn.regterschot@gmail.com>
2023-03-06 12:48:56 +0100

    feat: added hdri background

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -1,11 +1,13 @@
-import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { Environment, OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { Suspense } from "react";
 
 export function Scene() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
+      <Environment background={'both'} files={process.env.PUBLIC_URL + "/textures/sky-env.hdr"} />
+
       <PerspectiveCamera makeDefault position={[0, 0, 5]} fov={40} />
       <OrbitControls target={[0, 0, 0]}/>
     </Suspense>
-  )
+  );
 }
